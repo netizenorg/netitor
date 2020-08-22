8728065a75940af17468c550abbeff33a46ffd16
nbriz <nickbriz@gmail.com>
2020-08-22 18:16:35 -0500

    fixed _encode

--- src/main.js
+++ src/main.js
@@ -456,7 +456,7 @@ class Netitor {
   _findBestMatch (a, b) { return stringSimilarity.findBestMatch(a, b) }
 
   _decode (code) { return pako.inflate(window.atob(code), { to: 'string' }) }
-  _encode (code) { return pako.deflate(code, { to: 'string' }) }
+  _encode (code) { return pako.deflate(window.btoa(code), { to: 'string' }) }
 
   // •.¸¸¸.•*•.¸¸¸.•*•.¸¸¸.•*•.¸¸¸.•*•.¸¸¸.•*•.¸¸¸.•*•.¸¸¸.•*•.¸¸¸.•*•.¸¸¸.•*
   // •.¸¸¸.•*•.¸¸¸.•*•.¸¸¸.•*•.¸¸¸.•*•.¸¸¸.•*•.¸¸¸.•*•.¸¸¸.•*•.¸¸¸.•*  PUBLIC
@@ -522,7 +522,7 @@ class Netitor {
 
   saveToHash () {
     const data = this._encode(this.code)
-    window.location.hash = '#code/' + window.btoa(data)
+    window.location.hash = '#code/' + data
     return window.btoa(data)
   }
 
