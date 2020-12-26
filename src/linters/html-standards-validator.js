fe2fa6102390ba4822bf34277cf3540b8a9bf161
nbriz <nickbriz@gmail.com>
2020-12-26 15:58:37 -0600

    added missing language key in err object

--- src/linters/html-standards-validator.js
+++ src/linters/html-standards-validator.js
@@ -12,6 +12,7 @@ class HTMLStandards {
   }
 
   static verifyStandardElements (doc, code) {
+    const language = 'html'
     const errz = []
     let line = 0
     let col = 0
@@ -48,7 +49,7 @@ class HTMLStandards {
 
         const friendly = `${htmlMsg}. ${suggest}${fmsg}`
         const evidence = name
-        errz.push({ type, message, friendly, evidence, col, line, rule })
+        errz.push({ language, type, message, friendly, evidence, col, line, rule })
       }
     }
     return errz
@@ -67,6 +68,7 @@ class HTMLStandards {
 
     for (let i = 0; i < doc.all.length; i++) {
       const element = doc.all[i].localName
+      const language = 'html'
       if (Object.prototype.hasOwnProperty.call(htmlEles, element)) {
         const attrs = doc.all[i].attributes
         for (let i = 0; i < attrs.length; i++) {
@@ -89,7 +91,7 @@ class HTMLStandards {
               const fmsg = `If you were trying to create your own <a href="https://developer.mozilla.org/en-US/docs/Learn/HTML/Howto/Use_data_attributes" target="_blank">custom data attribute</a> you need to write it like this: <code>data-${attr}</code>`
               const friendly = `${htmlMsg}. ${suggest}${fmsg}`
 
-              errz.push({ type, message, friendly, evidence, col, line, rule })
+              errz.push({ language, type, message, friendly, evidence, col, line, rule })
             }
           } else if (htmlAttr[attr].status !== 'standard') {
             let fmsg = 'It may be depreciated or obsolete.'
@@ -100,7 +102,7 @@ class HTMLStandards {
             }
             const friendly = `${htmlMsg}. ${fmsg}`
 
-            errz.push({ type, message, friendly, evidence, col, line, rule })
+            errz.push({ language, type, message, friendly, evidence, col, line, rule })
           }
         }
       }
@@ -110,6 +112,7 @@ class HTMLStandards {
 
   static verifyTrailingSlashes (doc, code) {
     const errz = []
+    const language = 'html'
     const type = 'warning'
     const message = 'void elements should not have trailing slashes'
     const friendly = 'It used to be common to include "<a href="https://developer.mozilla.org/en-US/docs/MDN/Contribute/Guidelines/Code_guidelines/HTML#Trailing_slashes" target="_blank">trailing slashes</a>" in void elements (aka singleton elements) like <code>&lt;br/&gt;</code> or <code>&lt;img/&gt;</code> but these days we avoid them, it\'s better to write them like <code>&lt;br&gt;</code> and <code>&lt;img&gt;</code> '
@@ -124,7 +127,7 @@ class HTMLStandards {
       const evidence = match
       const line = lines.indexOf(match) + 1
       const col = match.indexOf('/>')
-      errz.push({ type, message, friendly, evidence, col, line, rule })
+      errz.push({ language, type, message, friendly, evidence, col, line, rule })
     })
 
     return errz
@@ -133,6 +136,7 @@ class HTMLStandards {
   static verifyLanAttr (doc, code) {
     const errz = []
     const type = 'warning'
+    const language = 'html'
     const message = '<html> element should have lang attribute'
     const friendly = 'Your <code>&lt;html&gt;</code> element should include a <a href="https://developer.mozilla.org/en-US/docs/Web/HTML/Global_attributes/lang" target="_blank">lang</a> attribute for accessibility and search enginges, for example <code>&lt;html lang="en-US"&gt;</code>'
     const rule = {
@@ -149,7 +153,7 @@ class HTMLStandards {
         const evidence = match
         const line = lines.indexOf(match) + 1
         const col = match.indexOf('<html')
-        errz.push({ type, message, friendly, evidence, col, line, rule })
+        errz.push({ language, type, message, friendly, evidence, col, line, rule })
       }
     }
 
@@ -159,6 +163,7 @@ class HTMLStandards {
   static verifyCharset (doc, code) {
     const errz = []
     const type = 'warning'
+    const language = 'html'
     const message = '<meata charset=""> must be present in <head> tag.'
     const friendly = 'You are missing a <a href="https://developer.mozilla.org/en-US/docs/Glossary/character_encoding" target="_blank"><code>&lt;meta charset="utf-8"&gt;</code></a> element in your <code>&lt;head&gt;</code>. While it is not technically required, it\'s highly recommended. Use UTF-8 unless you have a very good reason not to; it will cover your character needs pretty much regardless of what language you are using in your document. In addition, you should always specify the characterset as early as possible within your HTML\'s <code>&lt;head&gt;</code>, as it protects against a rather nasty <a href="https://support.microsoft.com/en-us/help/928847/internet-explorer-uses-the-wrong-character-set-when-it-renders-an-html" target="_blank">Internet Explorer security vulnerability</a>.'
     const rule = {
@@ -186,7 +191,7 @@ class HTMLStandards {
         const evidence = ''
         const line = lines.indexOf(match) + 1
         const col = match.indexOf('<head')
-        errz.push({ type, message, friendly, evidence, col, line, rule })
+        errz.push({ language, type, message, friendly, evidence, col, line, rule })
       }
     }
 
