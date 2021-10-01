c286738f03210ed3c025095bb2f695165f658d98
nbriz <nickbriz@gmail.com>
2021-10-01 15:46:59 -0500

    added missing CSS functions and HTML attribute to custom data

---
+++ src/edu-data/custom/custom-html-attributes.js
@@ -0,0 +1,18 @@
+module.exports = {
+  allowfullscreen: {
+    keyword: {
+      html: '<a href="https://developer.mozilla.org/en-US/docs/Web/HTML/Element/iframe#attr-allowfullscreen/" target="_blank">allowfullscreen</a>',
+      text: 'allowfullscreen'
+    },
+    url: 'https://developer.mozilla.org/en-US/docs/Web/HTML/Element/iframe#attr-allowfullscreen/',
+    status: 'standard',
+    elements: {
+      html: '<a target="_blank" href="https://developer.mozilla.org/en-US/docs/Web/HTML/Element/iframe"><code>&lt;iframe&gt;</code></a>',
+      text: '<iframe>'
+    },
+    description: {
+      html: '<code>allowfullscreen</code> is an attribute specifically for <code>iframe</code> elements. Though <code>iframes</code> can be used to embed any other website into a page, it is often used to embed sites that stream videos (like YouTube or Vimeo), this attribute allows that these iframes to be displayed in fullscreen mode.',
+      text: 'allowfullscreen is an attribute specifically for iframe elements. Though iframes can be used to embed any other website into a page, it is often used to embed sites that stream videos (like YouTube or Vimeo), this attribute allows that these iframes to be displayed in fullscreen mode.'
+    }
+  }
+}
--- src/edu-data/custom/index.js
+++ src/edu-data/custom/index.js
@@ -2,6 +2,10 @@ const fs = require('fs')
 const path = require('path')
 const cssProps = require('../css-properties.json')
 const customCSSProps = require('./custom-css-properties.js')
+const cssFuncs = require('../css-functions.json')
+const customCSSFuncs = require('./custom-css-functions.js')
+const htmlAttrs = require('../html-attributes.json')
+const customHtmlAttrs = require('./custom-html-attributes.js')
 
 function save (dictionary, destination) {
   const str = JSON.stringify(dictionary, null, 2)
@@ -15,3 +19,5 @@ function updateDict (original, updated, filepath) {
 }
 
 updateDict(cssProps, customCSSProps, '../css-properties.json')
+updateDict(cssFuncs, customCSSFuncs, '../css-functions.json')
+updateDict(htmlAttrs, customHtmlAttrs, '../html-attributes.json')
