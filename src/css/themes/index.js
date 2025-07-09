f4cb5f192dd34e617f0c661bce5a108026a927a4
nbriz <nickbriz@protonmail.com>
2025-07-09 10:36:16 -0500

    added sonnenzimmer theme

--- src/css/themes/index.js
+++ src/css/themes/index.js
@@ -2,6 +2,7 @@ module.exports = {
   dark: require('./dark.js'),
   light: require('./light.js'),
   monokai: require('./monokai.js'),
+  sonnenzimmer: require('./sonnenzimmer.js'),
   'moz-dark': require('./moz-dark.js'),
   'moz-light': require('./moz-light.js')
 }
---
+++ src/css/themes/sonnenzimmer.js
@@ -0,0 +1,42 @@
+
+module.exports = {
+  metadata: {
+    name: 'sonnenzimmer',
+    author: 'Nick Briz + Sonnenzimmer',
+    description: 'derived from the netizen.org *.O style guide'
+  },
+  /* THEME STARTS HERE */
+  text: '#ffffff',
+  background: '#000000',
+  /* CODE SYNTAX COLORS */
+  meta: '#ffffff',
+  tag: '#Ff99ff',
+  tag_bracket: '#ff99ff',
+  attribute: '#99ff99',
+  qualifier: '#99ff99',
+  comment: '#8c8c8c',
+  variable: '#ffffff',
+  variable_2: '#99ff99',
+  variable_3: '#ffffff',
+  variable_callee: '#ffffff',
+  def: '#99ff99',
+  builtin: '#99ff99',
+  property: '#99ffff',
+  keyword: '#ff9999',
+  string: '#ffff99',
+  string_2: '#ffff99',
+  number: '#9999ff',
+  atom: '#9999ff',
+  operator: '#ff99ff',
+  /* MISC EDITOR COLORS */
+  line_numbers: '#808080',
+  active_line_bg: '#404040',
+  selected: '#4d4d4d',
+  /* MATCHING TAG/BRACKET INDICATORS */
+  match_color: '#ffffff',
+  match_border: '#99ff99',
+  /* AUTOCOMPLETE/HINT MENU SELECTED ITEM */
+  hint_color: '#262626',
+  hint_bg: '#ffcc99',
+  hint_shadow: '#ffff99c9'
+}
